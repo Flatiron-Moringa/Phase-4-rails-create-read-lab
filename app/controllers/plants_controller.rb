c6b40bf62c1efa896cd45c289adec6a30f179e75
Maryann Mwikali <mwikali119@gmail.com>
2022-12-25 22:42:03 +0300

    Done

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,19 @@
+class PlantsController < ApplicationController
+    def index
+        render json: Plant.all, status: :ok
+    end
+
+    def show
+        plant = Plant.find_by(id:params[:id])
+        if plant
+            render json: plant, status: :ok
+        else
+            render json: {error: "Resource not found"}, status: :not_found
+        end
+    end
+
+    def create
+        plant = Plant.create(params.permit(:name, :image, :price))
+        render json: plant, status: :created
+    end
+end
